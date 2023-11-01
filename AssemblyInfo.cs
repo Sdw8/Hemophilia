8089eb34a4debb14c00d6608ec01e447440664ea
mavsd <mavsd@SMILE>
2023-11-01 19:19:09 +0300

    Добавьте файлы проекта.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hemophilia")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hemophilia")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4d39a6ed-8372-455b-b073-94aea1ae355f")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Properties/Resources.cs
@@ -0,0 +1,40 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Hemophilia.Properties
+{
+    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [DebuggerNonUserCode]
+    [CompilerGenerated]
+    internal class Resources
+    {
+        private static ResourceManager resourceMan;
+        private static CultureInfo resourceCulture;
+
+        internal Resources()
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (Hemophilia.Properties.Resources.resourceMan == null)
+                    Hemophilia.Properties.Resources.resourceMan = new ResourceManager("Hemophilia.Properties.Resources", typeof(Hemophilia.Properties.Resources).Assembly);
+                return Hemophilia.Properties.Resources.resourceMan;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get => Hemophilia.Properties.Resources.resourceCulture;
+            set => Hemophilia.Properties.Resources.resourceCulture = value;
+        }
+    }
+}
